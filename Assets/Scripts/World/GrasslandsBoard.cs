21bcc11b83e0cd3a3f212818d90f4f48b0bef3ac
Cody <54541144+CodyDickson@users.noreply.github.com>
2024-12-18 22:02:42 -0500

    GrasslandsBoard updated

--- Assets/Scripts/World/GrasslandsBoard.cs
+++ Assets/Scripts/World/GrasslandsBoard.cs
@@ -74,52 +74,48 @@ public class GrasslandsBoard : MonoBehaviour
 
     public static void LoopGenerator(int clockworkLocation, Tilemap tilemapBoardConnectors, Tile camp, Tile bcHorizontal, Tile bcThreeDown, Tile bcVertical, Tile bcThreeUp, Tile bcThreeLeft, Tile bcThreeRight, Tile bcTopRightCorner, Tile bcBottomLeftCorner, Tile bcBottomRightCorner, Tile bcTopLeftCorner)
     {
-        Vector3 midPositionOne = new Vector3(0, 0);
-        Vector3 cornerPositionOne = new Vector3(0, 0);
-        Vector3 midPositionTwo = new Vector3(0, 0);
-        Vector3 cornerPositionTwo = new Vector3(0, 0);
-        Vector3 midPositionThree = new Vector3(0, 0);
-        Vector3 cornerPositionThree = new Vector3(0, 0);
-        Vector3 midPositionFour = new Vector3(0, 0);
-        Vector3 cornerPositionFour = new Vector3(0, 0);
-        Vector3 connectorPosition = new Vector3(0, 0);
-        int startingLocation = clockworkLocation;
-        int shortSide = 2;
-        switch (startingLocation)
+        Vector3 center = new Vector3(0, 0);
+        switch (clockworkLocation)
         {
-            case 1: midPositionOne = new Vector3(0, 14); break;
-            case 2: midPositionOne = new Vector3(11, 14); break;
-            case 3: midPositionOne = new Vector3(11, 3); break;
-            case 4: midPositionOne = new Vector3(11, -8); break;
-            case 5: midPositionOne = new Vector3(0, -8); break;
-            case 6: midPositionOne = new Vector3(-11, -8); break;
-            case 7: midPositionOne = new Vector3(-11, 3); break;
-            case 8: midPositionOne = new Vector3(-11, 14); break;
-            case 9: midPositionOne = new Vector3(-11, 25); break;
-            case 10: midPositionOne = new Vector3(0, 25); break;
-            case 11: midPositionOne = new Vector3(11, 25); break;
-            case 12: midPositionOne = new Vector3(21, 25); break;
-            case 13: midPositionOne = new Vector3(21, 14); break;
-            case 14: midPositionOne = new Vector3(21, 3); break;
-            case 15: midPositionOne = new Vector3(21, -8); break;
-            case 16: midPositionOne = new Vector3(21, -18); break;
-            case 17: midPositionOne = new Vector3(11, -18); break;
-            case 18: midPositionOne = new Vector3(0, -18); break;
-            case 19: midPositionOne = new Vector3(-11, -18); break;
-            case 20: midPositionOne = new Vector3(-21, -18); break;
-            case 21: midPositionOne = new Vector3(-21, -8); break;
-            case 22: midPositionOne = new Vector3(-21, 3); break;
-            case 23: midPositionOne = new Vector3(-21, 14); break;
-            case 24: midPositionOne = new Vector3(-21, 25); break;
+            case 1: center = new Vector3(0, 13); break;
+            case 2: center = new Vector3(13, 13); break;
+            case 3: center = new Vector3(13, 0); break;
+            case 4: center = new Vector3(13, -13); break;
+            case 5: center = new Vector3(0, -13); break;
+            case 6: center = new Vector3(-13, -13); break;
+            case 7: center = new Vector3(-13, 0); break;
+            case 8: center = new Vector3(-13, 13); break;
+            case 9: center = new Vector3(-13, 26); break;
+            case 10: center = new Vector3(0, 26); break;
+            case 11: center = new Vector3(13, 26); break;
+            case 12: center = new Vector3(26, 26); break;
+            case 13: center = new Vector3(26, 13); break;
+            case 14: center = new Vector3(26, 0); break;
+            case 15: center = new Vector3(26, -13); break;
+            case 16: center = new Vector3(26, -26); break;
+            case 17: center = new Vector3(13, -26); break;
+            case 18: center = new Vector3(0, -26); break;
+            case 19: center = new Vector3(-13, -26); break;
+            case 20: center = new Vector3(-26, -26); break;
+            case 21: center = new Vector3(-26, -13); break;
+            case 22: center = new Vector3(-26, 0); break;
+            case 23: center = new Vector3(-26, 13); break;
+            case 24: center = new Vector3(-26, 26); break;
         }
-        cornerPositionOne = new Vector3(midPositionOne[0] + shortSide + 1, midPositionOne[1]);
-        midPositionTwo = new Vector3(cornerPositionOne[0], cornerPositionOne[1] - shortSide - 1);
-        cornerPositionTwo = new Vector3(midPositionTwo[0], midPositionTwo[1] - shortSide - 1);
-        midPositionThree = new Vector3(cornerPositionTwo[0] - shortSide - 1, cornerPositionTwo[1]);
-        cornerPositionThree = new Vector3(midPositionThree[0] - shortSide - 1, midPositionThree[1]);
-        midPositionFour = new Vector3(cornerPositionThree[0], cornerPositionThree[1] + shortSide + 1);
-        cornerPositionFour = new Vector3(midPositionFour[0], midPositionFour[1] + shortSide + 1);
-        int random;
+        Vector3 topLeftCorner = new Vector3(center[0] - 3, center[1] + 3);
+        Vector3 topRightCorner = new Vector3(center[0] + 3, center[1] + 3);
+        Vector3 bottomRightCorner = new Vector3(center[0] + 3, center[1] - 3);
+        Vector3 bottomLeftCorner = new Vector3(center[0] - 3, center[1] - 3);
+        tilemapBoardConnectors.SetTile(new Vector3Int((int)bottomLeftCorner[0], (int)bottomLeftCorner[1]), bcBottomLeftCorner);
+        tilemapBoardConnectors.SetTile(new Vector3Int((int)topLeftCorner[0], (int)topLeftCorner[1]), bcTopLeftCorner);
+        tilemapBoardConnectors.SetTile(new Vector3Int((int)topRightCorner[0], (int)topRightCorner[1]), bcTopRightCorner);
+        tilemapBoardConnectors.SetTile(new Vector3Int((int)bottomRightCorner[0], (int)bottomRightCorner[1]), bcBottomRightCorner);
+        World.boardPositions.Add(new Vector3((int)bottomLeftCorner[0], (int)bottomLeftCorner[1]));
+        World.boardPositions.Add(new Vector3((int)bottomRightCorner[0], (int)bottomRightCorner[1]));
+        World.boardPositions.Add(new Vector3((int)topRightCorner[0], (int)topRightCorner[1]));
+        World.boardPositions.Add(new Vector3((int)topLeftCorner[0], (int)topLeftCorner[1]));
+
+        /*int random;
         random = Random.Range(1, 3);
         if (random == 1)
         {
@@ -203,18 +199,7 @@ public class GrasslandsBoard : MonoBehaviour
             World.boardEmptySlotPositions.Add(new Vector3Int((int)midPositionTwo[0] - 1, (int)midPositionTwo[1] + i));
             World.boardEmptySlotPositions.Add(new Vector3Int((int)midPositionFour[0] + 1, (int)midPositionFour[1] + i));
         }
-        tilemapBoardConnectors.SetTile(new Vector3Int((int)cornerPositionThree[0], (int)cornerPositionThree[1]), bcBottomLeftCorner);
-        tilemapBoardConnectors.SetTile(new Vector3Int((int)cornerPositionFour[0], (int)cornerPositionFour[1]), bcTopLeftCorner);
-        tilemapBoardConnectors.SetTile(new Vector3Int((int)cornerPositionOne[0], (int)cornerPositionOne[1]), bcTopRightCorner);
-        tilemapBoardConnectors.SetTile(new Vector3Int((int)cornerPositionTwo[0], (int)cornerPositionTwo[1]), bcBottomRightCorner);
-        World.boardPositions.Add(new Vector3((int)midPositionOne[0], (int)midPositionOne[1]));
-        World.boardPositions.Add(new Vector3((int)cornerPositionOne[0], (int)cornerPositionOne[1]));
-        World.boardPositions.Add(new Vector3((int)midPositionTwo[0], (int)midPositionTwo[1]));
-        World.boardPositions.Add(new Vector3((int)cornerPositionTwo[0], (int)cornerPositionTwo[1]));
-        World.boardPositions.Add(new Vector3((int)midPositionThree[0], (int)midPositionThree[1]));
-        World.boardPositions.Add(new Vector3((int)cornerPositionThree[0], (int)cornerPositionThree[1]));
-        World.boardPositions.Add(new Vector3((int)midPositionFour[0], (int)midPositionFour[1]));
-        World.boardPositions.Add(new Vector3((int)cornerPositionFour[0], (int)cornerPositionFour[1]));
+
         if (startingLocation == 7)
         {
             tilemapBoardConnectors.SetTile(new Vector3Int((int)midPositionTwo[0], (int)midPositionTwo[1]), bcThreeRight);
@@ -238,7 +223,7 @@ public class GrasslandsBoard : MonoBehaviour
             tilemapBoardConnectors.SetTile(new Vector3Int((int)midPositionOne[0], (int)midPositionOne[1]), bcThreeUp);
             World.boardCrossroads.Add(new Vector3Int((int)midPositionOne[0], (int)midPositionOne[1]));
             World.boardEmptySlotPositions.Add(new Vector3Int((int)midPositionOne[0], (int)midPositionOne[1] - 1));
-        }
+        }*/
     }
 
     public static void BranchGenerator(int clockworkLocation, Tilemap tilemapBoardConnectors, Tile camp, Tile bcHorizontal, Tile bcThreeDown, Tile bcVertical, Tile bcThreeUp, Tile bcThreeLeft, Tile bcThreeRight, Tile bcTopRightCorner, Tile bcBottomLeftCorner, Tile bcBottomRightCorner, Tile bcTopLeftCorner)
